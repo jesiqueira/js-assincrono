8a243719cacffd8ddce835a743973722baea0837
Jose Edmar de Siqueira <jesiqueira@bbmapfre.com.br>
2023-06-21 19:09:43 -0300

    Promise e seus parametros

--- 1 - promises/script.js
+++ 1 - promises/script.js
@@ -1,13 +1,13 @@
-const promesa = new Promise((resolve, reject) => {
-  let condicao = false;
-  if (condicao) {
-    setTimeout(() => {
-      resolve({ nome: "edmar", idade: 38 });
-    }, 1000);
-  } else {
-    reject("Um erro ocorreu na promise");
-  }
-});
+// const promesa = new Promise((resolve, reject) => {
+//   let condicao = true;
+//   if (condicao) {
+//     setTimeout(() => {
+//       resolve({ nome: "edmar", idade: 38 });
+//     }, 1000);
+//   } else {
+//     reject("Um erro ocorreu na promise");
+//   }
+// });
 
 /*
 then()
@@ -15,22 +15,26 @@ O poder das Promises está no método then() do seu protótipo. O Callback desde
 resolve
 */
 
-const retorno = promesa
-  .then((resolucao) => {
-    // console.log(resolucao)
-    resolucao.profissao = "Analista";
-    return resolucao;
-  })
-  .then((resolucao) => {
-    resolucao.salario = 1526;
-    return resolucao;
-  })
-  .then((resolucao) => {
-    console.log(resolucao);
-  })
-  .catch((rejeitado) => {
-    console.log(rejeitado);
-  });
+// const retorno = promesa
+//   .then((resolucao) => {
+//     // console.log(resolucao)
+//     resolucao.profissao = "Analista";
+//     return resolucao;
+//   })
+//   .then((resolucao) => {
+//     resolucao.salario = 1526;
+//     return resolucao;
+//   })
+//   .then((resolucao) => {
+//     console.log(resolucao);
+//   })
+//   .catch((rejeitado) => {
+//     // console.log('CAT');
+//     console.log(rejeitado);
+//   })
+//   .finally(() => {
+//     console.log("Executando Finally");
+//   });
 // console.log(retorno);
 
 /*
@@ -49,3 +53,36 @@ O valor do primeiro argumento de cada then, será o valor do retorno anterior
 catch()
 o Método catch(), do protótipo de Promises, adiciona um callback a promise que será ativada caso a mesma seja rejeita
 */
+
+/*
+finally()
+Executará a função anônima assim que a promessa acabar. A diferença do finally é que ele será executado independente do resultado, se for resolvido ou rejeitado.
+*/
+
+/*
+-------Methods para promise--------
+Retornará uma nova promise assim que todas as ori=omises dentro deka forem resolvidas ou pelo menos uma rejeitada. A resposta é uma array com as respostas de  cada promise.
+*/
+
+/*
+Promesse.race()
+*/
+
+const login = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve("Usuário logado");
+  }, 1000);
+});
+
+const dados = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve("Dados do usuário logado");
+  }, 1500);
+});
+
+// const carregouTudo = Promise.all([login, dados]);
+const carregouTudo = Promise.race([login, dados]);
+
+carregouTudo.then((resolvido) => {
+  console.log(resolvido);
+});
