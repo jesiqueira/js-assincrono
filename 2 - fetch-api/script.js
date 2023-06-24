3beb17cba1ce653e4a2a367b1eb9e24fb693e411
Jose edmar de siqueira <edmar.ade@gmail.com>
2023-06-24 06:47:31 -0300

    finalizado exercício sobre fetch

--- 2 - fetch-api/script.js
+++ 2 - fetch-api/script.js
@@ -23,13 +23,39 @@ function buscaCep(cep) {
     });
 }
 
-
 /*
 Utilizando a API https://blockchain.info/ticker retorne no DOM o valor de compra da bitcoin and reais. Atualize este valor a cada 30s
 */
 
+const btc = document.querySelector(".btc");
+
+function fetchBtc() {
+  fetch("https://blockchain.info/ticker")
+    .then((response) => response.json())
+    .then((btcJson) => {
+      // console.log(btcJson.BRL);
+      btc.innerText = ('R$' + btcJson.BRL.buy).replace('.', ',');
+    });
+}
+
+// setInterval(fetchBtc, 1000);
+fetchBtc() 
 
 /*
 Utilizando a API https://api.chucknorris.io/jokes/random retorne uma piada randomica do chucknorris, toda vez que clicar em próxima
 
 */
+const btnProxima = document.querySelector('.proxima')
+const paragrafoPiada = document.querySelector('.piada')
+btnProxima.addEventListener('click', puxarPiada)
+
+function puxarPiada(){
+  fetch('https://api.chucknorris.io/jokes/random ')
+  .then(r => r.json())
+  .then(piada => {
+    // console.log(piada.value);
+    paragrafoPiada.innerText = piada.value
+  })
+}
+
+
